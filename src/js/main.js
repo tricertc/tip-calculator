4a8045953a4101e0657a19b06bc20809d2699755
Tim Rice <timrice@gmail.com>
2021-07-23 15:21:18 -0400

    added input icons

--- src/js/main.js
+++ src/js/main.js
@@ -15,6 +15,15 @@ document.addEventListener('alpine:init', () =>{
       tip: 0.15,
       people: 5,
 
+      tipOptions: [
+        { text: '5%', value: 0.05 },
+        { text: '10%', value: 0.10 },
+        { text: '15%', value: 0.15 },
+        { text: '25%', value: 0.25 },
+        { text: '50%', value: 0.50 },
+        { text: 'Custom', value: 'custom' },
+      ],
+
       getSplitTip() {
         const result = this.people > 0
           ? (this.bill * this.tip) / this.people
